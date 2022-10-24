43b16764b8cad52d962ad61673fa0a2844feb771
DamyVegaLeiva <damyvegaleiva@gmail.com>
2022-10-24 18:14:13 -0500

    Cart adding products + numberOfProducts

--- script/app.js
+++ script/app.js
@@ -1,64 +1,94 @@
+// VARIABLES / CONSTANTES
 const buttonCart = document.querySelector("#buttonCart");
-const myVinylsContenedor = document.querySelector(".row-vinyls");
-const tableBody = document.querySelector("#table-body")
-const cartContainer = document.querySelector("#modal-container")
-const searchBar = document.querySelector("#searchBar")
+const myVinylsContainer = document.querySelector(".render-vinyls");
+const tableBody = document.querySelector("#table-body");
+const cartContainer = document.querySelector("#modal-container");
+const searchBar = document.querySelector("#searchBar");
+
+// CART ARRAY
 const cart = [];
 
+// CREA ELEMENTOS POR CARA ITEM DEL ARRAY.
 function vinylsEl() {
-    myVinyls.forEach((myVinyls) => {
-        myVinylsContenedor.innerHTML += `
+    myVinyls.forEach((vinyl) => {
+        myVinylsContainer.innerHTML += `
         <article class="col-sm-12 col-md-6 col-lg-3">
-            <img src="${myVinyls.image}" alt="" class="w-100 img-thumbnail">
-            <h2 class="empanadas-sabores">${myVinyls.album}</h2>
-            <p>${myVinyls.artist}</p>
+            <img src="${vinyl.image}" alt="" class="w-100 img-thumbnail">
+            <h2 class="empanadas-sabores">${vinyl.album}</h2>
+            <p>${vinyl.artist}</p>
             <div class="options">
-                <h5>$${myVinyls.price}</h5>
-                <button id="addButton-${myVinyls.id}" class="btn btn-bg__color mb-2 w-100">Agregar</button>
+                <h5>$${vinyl.price}</h5>
+                <button id="addButton-${vinyl.id}" class="btn btn-bg__color mb-2 w-100">Agregar</button>
             </div>
         </article>
     `;
     });
 
     myVinyls.forEach((vinyl) => {
-        document.getElementById(`addButton-${vinyl.id}`).addEventListener("click", function () {
-            addToCart(vinyl)
-        })
-    })
+        document
+            .getElementById(`addButton-${vinyl.id}`)
+            .addEventListener("click", function () {
+                addToCart(vinyl);
+            });
+    });
 }
 
+// PUSH ITEMS AL CARRITO
 function addToCart(cartItem) {
-    cart.push(cartItem)
-    alert(`"${cartItem.artist} - ${cartItem.album}" was added to your cart.`)
 
-    tableBody.innerHTML += `
+    if (cart.find((item) => item.id == cartItem.id)) {
+        alert("Item already exists.");
+        cart.map((item) => {
+            let numberOfUnits = item.numberOfUnits;
+            numberOfUnits++;
+            item.numberOfUnits = numberOfUnits
+        })
+
+    } else {
+        cart.push({
+            ...cartItem,
+            numberOfUnits: 1,
+        });
+        alert(`"${cartItem.artist
+            } - ${cartItem.album}" was added to your cart.`);
+
+        tableBody.innerHTML += `
         <tr>
             <th>${cartItem.id}</th>    
             <th>${cartItem.artist} - ${cartItem.album}</th>
+            <th>1</th>
             <th>$${cartItem.price}</th>
         </tr>
     `;
 
-    let totalCarrito = cart.reduce((acumulador, prod) => acumulador + prod.price, 0);
-    document.getElementById("total").innerText = "Total a pagar $: " + totalCarrito;
-    document.getElementById("itemCounterCart").innerText = cart.length;
+        let cartTotal = cart.reduce(
+            (acumulador, prod) => acumulador + prod.price,
+            0
+        );
+        document.getElementById("total").innerText =
+            "Total a pagar $: " + cartTotal.toFixed(2);
+        document.getElementById("itemCounterCart").innerText = cart.length;
+    }
+    console.table(cart);
 }
 
 function showHideCart() {
-    if (cartContainer.style.display !== "none") {
-        cartContainer.style.display = "none"
+    if (cartContainer.classList.contains("dont-show")) {
+        cartContainer.classList.remove("dont-show")
     } else {
-        cartContainer.style.display = "block"
+        cartContainer.classList.add("dont-show")
     }
 }
 
+// FILTRADO DE BUSQUEDA POR ARTISTA
 function searchFilterItem() {
+    let searchBarText = searchBar.value.toUpperCase();
+    let filterSeach = myVinyls.filter((vinyl) =>
+        vinyl.artist.toUpperCase().includes(searchBarText)
+    );
 
-    let searchBarText = searchBar.value.toUpperCase()
-    let busquedaFiltrada = myVinyls.filter((vinyl) => vinyl.artist.toUpperCase().includes(searchBarText));
-
-    busquedaFiltrada.forEach((myVinyls) => {
-        myVinylsContenedor.innerHTML = `
+    filterSeach.forEach((myVinyls) => {
+        myVinylsContainer.innerHTML = `
             <article class="col-sm-12 col-md-6 col-lg-3">
                 <img src="${myVinyls.image}" alt="" class="w-100 img-thumbnail">
                 <h2 class="empanadas-sabores">${myVinyls.album}</h2>
@@ -70,13 +100,7 @@ function searchFilterItem() {
             </article>
         `;
     });
-    console.log(searchBarText);
 }
-
-vinylsEl();
-showHideCart()
-buttonCart.onclick = showHideCart;
-searchBar.addEventListener("keypress", capturarEnter)
 
 function capturarEnter(e) {
     if (e.which == 13 || e.keycode == 13) {
@@ -84,3 +108,7 @@ function capturarEnter(e) {
         e.preventDefault();
     }
 }
+
+vinylsEl();
+buttonCart.onclick = showHideCart;
+searchBar.addEventListener("keypress", capturarEnter);
--- script/products.js
+++ script/products.js
@@ -13,90 +13,106 @@ class Record {
 
 const myVinyls = [];
 
-myVinyls.push(new Record(
-  1,
-  "The Ramones",
-  "Road To Ruin (Vinyl)",
-  24.99,
-  "Punk",
-  20,
-  "./images/productos/ramones-roadtoruin.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    1,
+    "The Ramones",
+    "Road To Ruin (Vinyl)",
+    24.99,
+    "Punk",
+    20,
+    "./images/productos/ramones-roadtoruin.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  2,
-  "The Clash",
-  "Combat Rock (Vinyl)",
-  28.99,
-  "Punk, Rock",
-  20,
-  "./images/productos/theclash-combatrock.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    2,
+    "The Clash",
+    "Combat Rock (Vinyl)",
+    28.99,
+    "Punk, Rock",
+    20,
+    "./images/productos/theclash-combatrock.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  3,
-  "The Specials",
-  "The Specials (Vinyl)",
-  19.99,
-  "Ska",
-  15,
-  "./images/productos/thespecials-.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    3,
+    "The Specials",
+    "The Specials (Vinyl)",
+    19.99,
+    "Ska",
+    15,
+    "./images/productos/thespecials-.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  4,
-  "The Strokes",
-  "New Abnormal (Vinyl)",
-  30.99,
-  "Indie Rock",
-  10,
-  "./images/productos/thestrokes-thenewabnormal.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    4,
+    "The Strokes",
+    "New Abnormal (Vinyl)",
+    30.99,
+    "Indie Rock",
+    10,
+    "./images/productos/thestrokes-thenewabnormal.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  5,
-  "Brian Fallon",
-  "Sleepwalkers (Double Vinyl)",
-  34.99,
-  "Acoustic",
-  10,
-  "./images/productos/brian-fallon-sleepwalkers.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    5,
+    "Brian Fallon",
+    "Sleepwalkers (Double Vinyl)",
+    34.99,
+    "Acoustic",
+    10,
+    "./images/productos/brian-fallon-sleepwalkers.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  6,
-  "The Velvet Underground",
-  "Velvet Underground & Nico (Vinyl)",
-  33.99,
-  "Pop",
-  10,
-  "./images/productos/velvetunderground-nicoavif.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    6,
+    "The Velvet Underground",
+    "Velvet Underground & Nico (Vinyl)",
+    33.99,
+    "Pop",
+    10,
+    "./images/productos/velvetunderground-nicoavif.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  7,
-  "The Gaslight Anthem",
-  "Handwritten (Vinyl)",
-  27.99,
-  "Alternative Rock",
-  10,
-  "./images/productos/the-gaslight-anthem-handwritten.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    7,
+    "The Gaslight Anthem",
+    "Handwritten (Vinyl)",
+    27.99,
+    "Alternative Rock",
+    10,
+    "./images/productos/the-gaslight-anthem-handwritten.avif",
+    "Vinyl"
+  )
+);
 
-myVinyls.push(new Record(
-  8,
-  "Rocky Votolato",
-  "Television Of Saints (Vinyl)",
-  23.99,
-  "Acoustic",
-  10,
-  "./images/productos/rocky-votolato-television-of-saints.avif",
-  "Vinyl"
-));
+myVinyls.push(
+  new Record(
+    8,
+    "Rocky Votolato",
+    "Television Of Saints (Vinyl)",
+    23.99,
+    "Acoustic",
+    10,
+    "./images/productos/rocky-votolato-television-of-saints.avif",
+    "Vinyl"
+  )
+);
