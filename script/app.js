2e431ebd68690a244a5b01897d1a37ebc2084791
DamyVegaLeiva <damyvegaleiva@gmail.com>
2022-10-29 15:49:25 -0500

    Update carrito

--- script/app.js
+++ script/app.js
@@ -3,8 +3,7 @@ const buttonCart = document.querySelector("#buttonCart");
 const myVinylsContainer = document.querySelector(".render-vinyls");
 const tableBody = document.querySelector("#table-body");
 const cartContainer = document.querySelector("#modal-container");
-const searchBar = document.querySelector("#searchBar");
-
+const searchInput = document.querySelector("#searchInput");
 
 // CART ARRAY.
 const cart = [];
@@ -38,79 +37,128 @@ function vinylsEl() {
 function addToCart(cartItem) {
     //AGREGA UNIDAD DE PRODUCTO SI ITEM YA EXISTE EN CARRITO
     if (cart.find((item) => item.id == cartItem.id)) {
-        cart.find((item) => {
-            if (cartItem.id == item.id && item.numberOfUnits) {
-                item.numberOfUnits++;
-                item.price += cartItem.price
-                document.getElementById(`cantidad-product-${cartItem.id}`).innerText = `${item.numberOfUnits}`
-                document.getElementById(`total-prod-${cartItem.id}`).innerText = `${item.price.toFixed(2)}`
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Item already exists in cart.",
+            icon: 'question',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, add it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                cart.find((item) => {
+                    if (cartItem.id == item.id && item.numberOfUnits) {
+                        item.numberOfUnits++;
+                        item.price += cartItem.price;
+                        document.getElementById(
+                            `cantidad-product-${cartItem.id}`
+                        ).innerText = `${item.numberOfUnits}`;
+                        document.getElementById(
+                            `total-prod-${cartItem.id}`
+                        ).innerText = `${item.price.toFixed(2)}`;
+                        updateCart();
+                    }
+                });
             }
-        });
+        })
         //AGREGAR ITEM POR PRIMERA AL CARRITO
     } else {
         cart.push({
             ...cartItem,
             numberOfUnits: 1,
         });
-
-        alert(`"${cartItem.artist
-            } - ${cartItem.album}" was added to your cart.`);
-
+        //ALER ITEM AGREGADO AL CARRITO
+        Swal.fire({
+            title: `${cartItem.artist} - ${cartItem.album}`,
+            text: 'Was added to the cart.',
+            imageUrl: cartItem.image,
+            imageWidth: 200,
+            imageHeight: 200,
+            imageAlt: `${cartItem.artist} - ${cartItem.album}`,
+            backdrop: 'swal2-backdrop-show',
+            showClass: {
+                popup: 'animate__animated animate__fadeInDown'
+            },
+            hideClass: {
+                popup: 'animate__animated animate__fadeOutUp'
+            }
+        })
         tableBody.innerHTML += `
-        <tr>
+        <tr id="row-item-${cartItem.id}">
             <th>${cartItem.id}</th>    
             <th>${cartItem.artist} - ${cartItem.album}</th>
             <th id="cantidad-product-${cartItem.id}">1</th>
             <th id="total-prod-${cartItem.id}">$${cartItem.price}</th>
+            <th id="remove-item-${cartItem.id}">X</th>
         </tr>
     `;
+        //ELIMINAR ITEM CARRITO EVENTO/CAPTURA
+        cart.forEach((vinyl) => {
+            document
+                .getElementById(`remove-item-${vinyl.id}`)
+                .addEventListener("click", () => {
+                    removeItemCart(vinyl);
+                });
+        });
     }
+    updateCart();
+}
 
-    //SUMA TOTAL A PAGAR DE ITEMS EN EL CARRITO 
+function removeItemCart(vinyl) {
+    cart.splice(cart.indexOf(vinyl), 1)
+    document.getElementById(`row-item-${vinyl.id}`).innerHTML = "";
+    updateCart();
+}
+
+//UPDATECART
+function updateCart() {
+    //SUMA TOTAL A PAGAR DE ITEMS EN EL CARRITO
     let cartTotal = cart.reduce((acumulador, prod) => acumulador + prod.price, 0);
-
     document.getElementById("total").innerText =
         "Total a pagar $: " + cartTotal.toFixed(2);
-    document.getElementById("itemCounterCart").innerText = cart.length;
-    // console.table(cart);
+    document.getElementById("itemCounterCart").innerText = cart.reduce(
+        (acumulador, prod) => acumulador + prod.numberOfUnits,
+        0
+    );
 }
 
 //MUESTRA O ESCONDE EL DIV DEL CARRITO
 function showHideCart() {
     if (cartContainer.classList.contains("dont-show")) {
-        cartContainer.classList.remove("dont-show")
+        cartContainer.classList.remove("dont-show");
     } else {
-        cartContainer.classList.add("dont-show")
+        cartContainer.classList.add("dont-show");
     }
 }
 
+//LIMPIA CONTENEDOR VINYLS
 function clearContainer() {
     myVinylsContainer.innerHTML = "";
 }
 
 // FILTRADO DE BUSQUEDA POR ARTISTA
 function searchFilterItem() {
-    let searchBarText = searchBar.value.toUpperCase();
+    let searchBarText = searchInput.value.toUpperCase();
     let filterSeach = myVinyls.filter((vinyl) =>
         vinyl.artist.toUpperCase().includes(searchBarText)
     );
-    //LIMPIA CONTENEDOR DE VINILOS 
+    //LIMPIA CONTENEDOR DE VINILOS
     clearContainer();
     //MUESTRA RESULTADO DE BUSQUEDA.
     filterSeach.forEach((myVinyls) => {
         myVinylsContainer.innerHTML += `
-            <article class="col-sm-12 col-md-6 col-lg-3">
-                <img src="${myVinyls.image}" alt="" class="w-100 img-thumbnail">
-                <h2 class="empanadas-sabores">${myVinyls.album}</h2>
-                <p>${myVinyls.artist}</p>
+        <article class= "col-sm-12 col-md-6 col-lg-3">
+        <img src="${myVinyls.image}" alt="" class="w-100 img-thumbnail">
+            <h2 class="empanadas-sabores">${myVinyls.album}</h2>
+            <p>${myVinyls.artist}</p>
                 <div class="options">
                     <h5>$${myVinyls.price}</h5>
                     <button id="addButton-${myVinyls.id}" class="btn btn-bg__color mb-2 w-100">Agregar</button>
                 </div>
-            </article>
+        </article>
         `;
     });
-    console.log(filterSeach);
 }
 
 function capturarEnter(e) {
@@ -122,4 +170,4 @@ function capturarEnter(e) {
 
 vinylsEl();
 buttonCart.onclick = showHideCart;
-searchBar.addEventListener("keypress", capturarEnter);
+searchInput.addEventListener("keypress", capturarEnter);
